f66e1de5765482645e8f75f467fca2ba0f2402da
rafa <zawkindev@gmail.com>
2026-04-30 15:24:07 +0500

    feat: bool, comment and letter parsers with tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use katou::parser::parse_single_line_comments;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Welcome to Katou - the Lisp Parser");
+    Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,35 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while},
+    character::complete::alpha0,
+    combinator::{eof, value},
+    sequence::preceded,
+    IResult, Parser,
+};
+use std::error::Error;
+
+pub fn parse_letters(input: &str) -> IResult<&str, &str> {
+    alpha0(input)
+}
+
+pub fn parse_bool(input: &str) -> IResult<&str, bool> {
+    alt((value(true, tag("true")), value(false, tag("false")))).parse(input)
+}
+
+pub fn parse_single_line_comments(input: &str) -> IResult<&str, &str> {
+    preceded(tag(";;"), take_while(|x| x != '\n' || x != '\0')).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn single_line_comment() -> Result<(), Box<dyn Error>> {
+        let (remaining, out) =
+            parse_single_line_comments(";; Single line comments start with a semicolon")?;
+        assert_eq!(out, " Single line comments start with a semicolon");
+        assert_eq!(remaining, "");
+        Ok(())
+    }
+}
